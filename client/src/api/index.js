57aeb8631e277e5b329c373c7d6a8e6ab717e31d
Subhashini29 <subhashini@agenciflow.com>
2026-04-10 12:01:41 +0530

    ENV changed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,4 @@
-// const BASE_URL =  import.meta.env.VITE_API_URL || "http://localhost:8001/api";
-const BASE_URL =  "http://localhost:8000/api";
+const BASE_URL =  import.meta.env.VITE_API_URL || "http://localhost:8001/api";
 
 
 export const apiRequest = async (endpoint, options = {}) => {
